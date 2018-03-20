419156c9d4bb20b05fe43681bde0a2513c05673e
Joanne <thecodingdiva@gmail.com>
2018-03-19 21:46:55 -0400

    Add project files.

---
+++ TriangleArea/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 1;
+            double b = 2;
+            double c = 3;
+
+            double p = (a + b + c) / 2;
+
+        }
+    }
+}
